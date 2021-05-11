589e94b3571adb236fc513fd43420fcdc9ac27d1
Noah <nallephu12@gmail.com>
2021-05-11 22:43:12 +0200

    Added support to take in commandline arguments

--- projekt/main.cpp
+++ projekt/main.cpp
@@ -1,8 +1,21 @@
 #include "adjList.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
+    if (argc != 3)
+    {
+        std::cout << "Wrong number of Command line arguments, expected 2 "
+                     ":)\n>>> ./algo.app <inFile.txt> <outFile.txt>"
+                  << std::endl;
+        return 0;
+    }
     adjList adjList;
     adjList.createAdjList("graph.txt");
     adjList.printList();
+    std::cout << argc << std::endl;
+    for (int i = 0; i < argc; i++)
+    {
+        std::cout << argv[i] << std::endl;
+    }
+    return 0;
 }
