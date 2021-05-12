819f092aa567a131fb9897b29b742649b6eefc31
Noah <nallephu12@gmail.com>
2021-05-12 18:26:16 +0200

    moved adjList files to own folder. Removed prim file from kruskals dir.

--- kruskals/include/Graph.hpp
+++ kruskals/include/Graph.hpp
@@ -1,6 +1,11 @@
 #ifndef GRAPH_HPP
 #define GRAPH_HPP
 
+struct ed
+{
+    // Code
+};
+
 class Kruskal
 {
 public:
@@ -10,4 +15,4 @@ public:
 private:
 };
 
-#endif /* GRAPH_H */
+#endif /* GRAPH_HPP */
